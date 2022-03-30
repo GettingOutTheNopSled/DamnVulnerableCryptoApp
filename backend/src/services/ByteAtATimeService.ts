e96cf736b318601982416f423b9c82d0535ff9ae
Matt <matt.ritter@upstart.com>
2022-03-30 10:21:44 -0400

    Yet more attempts...

--- backend/src/services/ByteAtATimeService.ts
+++ backend/src/services/ByteAtATimeService.ts
@@ -4,19 +4,15 @@ import { ChallengeService } from './ChallengeService';
 
 export class ByteAtATimeService extends ChallengeService {
   public static KEY = "3jc^aijs/jzn%nai";
-
-  public static encrypt(data: string): string {
-    const cipher = crypto.createCipheriv('aes-128-ecb', Buffer.from(this.KEY), '');
-    const tokenBuffer = Buffer.from(data, 'utf8');
-    let encrypted = cipher.update(tokenBuffer);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-
-    return encrypted.toString("hex");
-  }
+  public static SECONDKEY = "3jc^aijs/jzn%nai";
 
   public static decrypt(data: string): string {
-    const decipher = crypto.createDecipheriv('aes-128-ecb', Buffer.from(this.KEY), '');
-    const encryptedText = Buffer.from(data, 'hex');
+    const decipher = crypto.createDecipheriv(
+      "aes-128-ecb",
+      Buffer.from(this.KEY),
+      ""
+    );
+    const encryptedText = Buffer.from(data, "hex");
     let decrypted = decipher.update(encryptedText);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
 
@@ -24,5 +20,18 @@ export class ByteAtATimeService extends ChallengeService {
     $log.info("Decrypted token: " + t);
 
     return t;
+  }
+
+  public static encrypt(data: string): string {
+    const cipher = crypto.createCipheriv(
+      "aes-128-ecb",
+      Buffer.from(this.SECONDKEY),
+      ""
+    );
+    const tokenBuffer = Buffer.from(data, "utf8");
+    let encrypted = cipher.update(tokenBuffer);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+    return encrypted.toString("hex");
   }
 }
